90b239aedfdb74bca1f39d4575f861a4ca9d10f0
LouieSF <lsf2004impo@gmail.com>
2024-07-26 21:10:30 -0400

    added decode function

--- Lab 9/afs_lab9.py
+++ Lab 9/afs_lab9.py
@@ -9,3 +9,15 @@ def encode(password):
 #test case
 print(encode("12345555"))
 print(encode("00009962"))
+
+def decode(password):
+    encode = ""
+    for i in password:
+        if "0" <= i <= "9":
+            encoded_i = str((int(i) - 3) % 10)
+            encode += encoded_i
+    return encode
+
+#test case
+print(decode("45678888"))
+print(decode("33332295"))
